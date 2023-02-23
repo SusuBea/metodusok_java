b123f7600c7beb72e9acb064cfb83a12e68ec730
SuSuBea <suhajdabea@gmail.com>
2023-02-23 18:04:45 +0100

    2feladat

--- Metodusok.java
+++ Metodusok.java
@@ -5,20 +5,33 @@ package metodusok;
 public class Metodusok {
 
     public static void main(String[] args) {
+        int osszeg = elso10szamOsszege();
+        String kimenet = "Az első 10 szám összege: " + osszeg + "\n)";
+        kiir(kimenet);
+        
+        int szam1 = 7, szam2 = 4;
+        kiir("%d + %d = %d\n".formatted(szam1, szam2, szam1+szam2));
+        
        
     }
     
-        public static void elso10szamOsszege() {
+        public static int elso10szamOsszege() {
             int osszeg = 0;
-            for(int i =0; i < 10; i++){
+            for(int i =0; i < 10; i++) {
                 osszeg += i;
             }
+            return osszeg;
             
-            System.out.println("Az első 10 szám összege: +" + osszeg);
+            //System.out.println("Az első 10 szám összege: +" + osszeg);
     }
     
-        public static void osszead(int a, int b){
-            System.out.printf("%d + %d = %d\n", a, b, a+b);
+        public static int osszead(int a, int b){
+            //System.out.printf("%d + %d = %d\n", a, b, a+b);
+            return a+b;
+        }
+        
+        public static void kiir(String szoveg){
+            System.out.print(szoveg);
         }
         
 }
