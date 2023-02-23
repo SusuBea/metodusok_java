ecad8cd89ff5efd515c6eb18c65d2d86fadb8e16
SuSuBea <suhajdabea@gmail.com>
2023-02-23 18:49:17 +0100

    4szam

--- Metodusok.java
+++ Metodusok.java
@@ -5,13 +5,22 @@ package metodusok;
 public class Metodusok {
 
     public static void main(String[] args) {
-        int osszeg = osszead();
-        String kimenet = "4 szám összege: " + osszeg + "\n";
+        int osszeg = elso10szamOsszege();
+        String kimenet = "Az első 10 szám összege: " + osszeg + "\n";
         kiir(kimenet);
         
-        int szam1 = 7, szam2 = 4, szam3 = 5, szam4= 9;
-        kiir("%d + %d + %d + %d = %d\n".formatted(szam1, szam2, szam3, szam4, osszeg));
+        int szam1 = 7, szam2 = 4;
+        kiir("%d + %d  = %d\n".formatted(szam1, szam2,  osszeg));
         
+        osszeg = osszead(1, 2);
+        osszeg = osszead(osszeg, 3);
+        osszeg = osszead(osszeg, 4);
+        kiir("1+2+3+4="+osszeg+"\n");
+        
+        
+        
+        
+                
        
     }
     
@@ -25,9 +34,9 @@ public class Metodusok {
             //System.out.println("Az első 10 szám összege: +" + osszeg);
     }
     
-        public static int osszead(int a, int b, int c, int d){
+        public static int osszead(int a, int b){
             //System.out.printf("%d + %d = %d\n", a, b, a+b);
-            return a+b+c+d;
+            return a+b;
         }
         
         public static void kiir(String szoveg){
